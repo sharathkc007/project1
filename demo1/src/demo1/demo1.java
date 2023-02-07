34b5e33168c5f8dc7d89b1e4652bcc630440eaaf
User <User@DELL>
2023-02-07 17:52:52 +0530

    please rewiev

---
+++ demo1/src/demo1/demo1.java
@@ -0,0 +1,14 @@
+package demo1;
+
+public class demo1 
+{
+	public final static int a=100;
+	public final static int b=10;
+	
+	
+  
+ 
+ 
+ 
+
+}
---
+++ demo1/src/demo2/demo2.java
@@ -0,0 +1,20 @@
+package demo2;
+
+import demo1.demo1;
+
+public class demo2
+{
+
+	public static void main(String[] args) 
+	{
+	
+ 
+ int c= demo1.a+demo1.b;
+ System.out.println(c);
+
+  
+ 
+ 
+	}
+
+}
